de887ccca52fb7f29ed170c7fb3daa13498648f1
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-08-03 16:19:37 -0400

    Add project files.

---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,192 @@
+﻿using DinoMatchGame.DAL;
+using DinoMatchGame.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DinoMatchGame.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Guardar(Usuarios usuarios)
+        {
+            if (!Existe(usuarios.UsuarioId))
+                return Insertar(usuarios);
+            else
+                return Modificar(usuarios);
+        }
+
+        private static bool Existe(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                paso = contexto.usuarios.Any(x => x.UsuarioId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Insertar(Usuarios usuarios)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.usuarios.Add(usuarios);
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Modificar(Usuarios usuarios)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(usuarios).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var ventas = Buscar(id);
+
+                contexto.Entry(ventas).State = EntityState.Deleted;
+                paso = (contexto.SaveChanges() > 0);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuarios;
+
+            try
+            {
+                usuarios = contexto.usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return usuarios;
+        }
+
+        public static bool ExisteUsuario(string NombreDeUsuario)
+        {
+            bool encontrado = false;
+            var contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.usuarios.Any(e => e.NombreDeUsuario == NombreDeUsuario );
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        public static List<Usuarios> GetUsuarios()
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+            Contexto db = new Contexto();
+            try
+            {
+                lista = db.usuarios.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> criterio)
+        {
+            List<Usuarios> Lista = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.usuarios.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+        
+
+        
+    }
+}
+
---
+++ Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinoMatchGame.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string NombreCompleto { get; set; }
+        public string NombreDeUsuario { get; set; }
+        public string MejorPuntuación { get; set; }
+        public int VecesJugado { get; set; }
+
+
+    }
+}
